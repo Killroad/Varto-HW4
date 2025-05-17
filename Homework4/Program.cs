b4dcd216c9b406c13668a01cf2aad10897807cef
Valerii N <killroad90@gmail.com>
2025-05-17 17:18:16 +0300

    feat: implement even number array generator

--- Homework4/Program.cs
+++ Homework4/Program.cs
@@ -7,7 +7,22 @@ namespace Homework4
     {
         static void Main()
         {
+            GenerateEvenNumbersArray();
+        }
+        
+        static void GenerateEvenNumbersArray()
+        {
+            int[] evenNumbers = new int[50];
             
+            for (int i = 0, j = 2; i < evenNumbers.Length; i++, j += 2)
+            {
+                evenNumbers[i] = j;
+            }
+
+            foreach (int number in evenNumbers)
+            {
+                Console.WriteLine($"Number: {number}");
+            }
         }
     }
 }
