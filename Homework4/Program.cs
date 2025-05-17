e1a2b00ac40f47e75ed5ec053ed24e583e98f2a3
Valerii N <killroad90@gmail.com>
2025-05-17 17:22:11 +0300

    feat: add method to count and list even numbers in random array

--- Homework4/Program.cs
+++ Homework4/Program.cs
@@ -7,7 +7,23 @@ namespace Homework4
     {
         static void Main()
         {
+            CountEvenRandomNumbers();
+        }
+        
+        static void CountEvenRandomNumbers()
+        {
+            int[] numbers = new int[40];
+            List<int> evenNumbers = new List<int>();
+            Random rng = new Random();
             
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = rng.Next(1, 500);
+                if (numbers[i] % 2 == 0) 
+                    evenNumbers.Add(numbers[i]);
+            }
+            Console.WriteLine($"Array contains {evenNumbers.Count} even number(s).");
+            Console.WriteLine($"Even numbers: {string.Join(", ", evenNumbers)}.");
         }
     }
 }
