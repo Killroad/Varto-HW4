1d95c3ad84f8d217292780fdafa2acc9d4ad8353
Valerii N <killroad90@gmail.com>
2025-05-17 17:01:48 +0300

    Init commit

---
+++ Homework4/Program.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+
+namespace Homework4
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            
+        }
+    }
+}
